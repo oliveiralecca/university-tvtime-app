49f2ba275faee9a2b66e7445ca995b16603b582f
oliveiralecca <oliveiraslc@yahoo.com.br>
2022-10-21 22:51:54 -0300

    docs: sets db URL in private comment

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,3 @@
 console.log('Hello world!')
+
+//DATABASE_URL = postgres://vehbmjkxyehjva:ddb3629b93fe6345b4a2b61ec9540ef1e365a472b894a5ea21ea1fca523e9476@ec2-54-147-36-107.compute-1.amazonaws.com:5432/d8fibfmu8piur8
