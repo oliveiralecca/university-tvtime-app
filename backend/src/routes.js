ba7a8486fb694c34321ac6f865173d468bacb35f
Rodrigo Nunes de Santana <76262266+rodrigodesan@users.noreply.github.com>
2022-11-11 00:18:52 -0300

    feat(routes): add routes from filme and genero

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,21 +1,19 @@
 const express = require('express');
 const route = express.Router();
 
-const homeController = require('./controllers/homeController');
-const filmeController = require('./controllers/filmeController')
+const filmeController = require('./controllers/filmeController');
+const generoController = require('./controllers/generoController')
 
 
-route.get('/', homeController.index);
+route.get("/filme/index", filmeController.index);
+route.post("/filme/register", filmeController.register);
+route.get("/filme/list", filmeController.list);
+route.get("/filme/list/:id_filme", filmeController.single);
 
 
-route.get('/users', homeController.userList);
-route.get("/users/:user", homeController.findUser);
-route.put("/users/:user", homeController.udateUser);
-route.delete("/users/:user", homeController.udateUser);
-
-route.get("/filmes", filmeController.index);
-route.post("/filmes/create", filmeController.createFilme);
-route.get("/turma/turmas", filmeController.turmas);
-route.get("/turma/turmas/:id_turma", filmeController.turma);
+route.get("/genero/index", generoController.index);
+route.post("/genero/register", generoController.register);
+route.get("/genero/list", generoController.list)
+route.get("/genero/list/:id_genero", generoController.single);
 
 module.exports = route;
