c09851ae09930ec16bd3d534af3e486579ec06c0
Rodrigo Nunes de Santana <76262266+rodrigodesan@users.noreply.github.com>
2022-11-11 23:22:47 -0300

    feat(prisma): new database connection

--- backend/src/server.js
+++ backend/src/server.js
@@ -19,4 +19,4 @@ app.use(routes);
 app.listen(3030, () => console.log('Server listening on http://localhost:3030/'))
 
 
-//databaseURL = postgres://lhheztjzyyzlds:b9e460e53628e5ac01dcdc1bfb1f63ed23d51aa9104684893684f0c46676fad9@ec2-54-160-200-167.compute-1.amazonaws.com:5432/d6gu1h1g5eq4tf?schema=tvtime
+//DATABASE_URL="postgres://vtkegxfd:AjrKUMY5YiFS5G_qwGtrkP3JGfQyu-6A@babar.db.elephantsql.com/vtkegxfd?schema=tvtime"
