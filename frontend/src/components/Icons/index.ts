427ee39aee037db7a1565413a2663d609c408d8d
oliveiralecca <oliveiraslc@yahoo.com.br>
2022-11-12 04:44:57 -0300

    style(icons): adds svg icons

---
+++ frontend/src/components/Icons/index.ts
@@ -0,0 +1,7 @@
+import { ReactComponent as AllIcon } from "../../assets/icons/squares.svg";
+import { ReactComponent as EditorIcon } from "../../assets/icons/pencil.svg";
+import { ReactComponent as GenresIcon } from "../../assets/icons/ghost.svg";
+import { ReactComponent as MovieIcon } from "../../assets/icons/movie.svg";
+import { ReactComponent as TvIcon } from "../../assets/icons/tv.svg";
+
+export { AllIcon, EditorIcon, GenresIcon, MovieIcon, TvIcon };
