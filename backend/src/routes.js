1a15de624dacd1c874a3bd1e8373c7dc56218d93
Rodrigo Nunes de Santana <76262266+rodrigodesan@users.noreply.github.com>
2022-11-12 19:05:04 -0300

    feat(genero): icon upload middleware

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const route = express.Router();
 
+// Controllers
 const filmeController = require('./controllers/filmeController');
 const generoController = require('./controllers/generoController');
 
-const capaUpload = require('./middlewares/capaUpload')
+// Middlewares
+const capaUpload = require('./middlewares/capaUpload');
+const iconeUpload = require('./middlewares/iconeUpload');
 
-
+// Rotas de filme
 route.get("/filme/index", filmeController.index);
 route.post("/filme/register", capaUpload, filmeController.register);
 route.get("/filme/list", filmeController.list);
@@ -14,11 +17,12 @@ route.get("/filme/list/:id_filme", filmeController.single);
 route.put("/filme/update/:id_filme",capaUpload,filmeController.update);
 route.delete("/filme/delete/:id_filme",filmeController.delete);
 
+// Rotas de genero
 route.get("/genero/index", generoController.index);
-route.post("/genero/register", generoController.register);
+route.post("/genero/register", iconeUpload, generoController.register);
 route.get("/genero/list", generoController.list)
 route.get("/genero/list/:id_genero", generoController.single);
-route.put("/genero/update/:id_genero",generoController.update);
+route.put("/genero/update/:id_genero",iconeUpload, generoController.update);
 route.delete("/genero/delete/:id_genero",generoController.delete);
 
 
