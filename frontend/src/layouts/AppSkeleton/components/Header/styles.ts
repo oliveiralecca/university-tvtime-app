f91f8dca9b4887c19d6cf269dc42b90a79eb7ce4
oliveiralecca <oliveiraslc@yahoo.com.br>
2022-11-12 16:32:40 -0300

    refactor(layout): updates `Header` component

---
+++ frontend/src/layouts/AppSkeleton/components/Header/styles.ts
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const Container = styled.header`
+  flex: 0 50px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const Title = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 20px;
+`;
+
+export const Separator = styled.div`
+  border-left: 2px solid #211f4180;
+  height: 40px;
+`;
+
+export const ProfilePhoto = styled.img`
+  height: 30px;
+  width: 30px;
+  padding: 10px;
+
+  border-radius: 20px;
+
+  background-color: #fe7141;
+`;
