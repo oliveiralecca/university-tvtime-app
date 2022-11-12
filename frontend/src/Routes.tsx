30d22becc1b28367b920e7a6e4b717f44eb47114
oliveiralecca <oliveiraslc@yahoo.com.br>
2022-11-12 15:25:49 -0300

    feat(routes): adds redirect route from `/` to `/home`

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 
 import { Dashboard } from "./pages/dashboard";
 import { RegisterForm } from "./pages/form";
@@ -11,23 +11,35 @@ export function AppRoutes() {
     <Router>
       <Routes>
         <Route
-          path="/"
+          path="/home"
           element={<AppSkeleton children={<Dashboard />} title="Dashboard" />}
         />
+        <Route path="/" element={<Navigate to="/home" />} />
         <Route
           path="/cadastrar"
-          element={<AppSkeleton children={<p>Rota com filmes cadastrados e opção de editar + botão para form de cadastro</p>} title="Filmes cadastrados" />}
+          element={
+            <AppSkeleton
+              children={
+                <p>
+                  Rota com filmes cadastrados e opção de editar + botão para
+                  form de cadastro
+                </p>
+              }
+              title="Filmes cadastrados"
+            />
+          }
         />
         <Route
           path="/cadastrar/novo"
-          element={<AppSkeleton children={<RegisterForm />} title="Cadastrar filme" />}
+          element={
+            <AppSkeleton children={<RegisterForm />} title="Cadastrar filme" />
+          }
         />
         {/* <Route path="/details/:user" element={<Details />} /> */}
         <Route 
           path="/teste/inicio"
           element={<AppSkeleton children= {<MovieDetails/>} title="Teste"/>}
-          />
-        
+          />       
         <Route path="*" element={<Page404 />} />
       </Routes>
     </Router>
