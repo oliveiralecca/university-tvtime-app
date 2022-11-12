b0590f63389f31e8524befefab979212ff894fb0
oliveiralecca <oliveiraslc@yahoo.com.br>
2022-11-12 04:48:47 -0300

    refactor(navbar): updates navbar component

--- frontend/src/layouts/AppSkeleton/components/NavBar/index.tsx
+++ frontend/src/layouts/AppSkeleton/components/NavBar/index.tsx
@@ -1,3 +1,26 @@
+import {
+  AllIcon,
+  EditorIcon,
+  GenresIcon,
+  MovieIcon,
+  TvIcon,
+} from "../../../../components/Icons";
+import { NavButton } from "../../../../components/NavButton";
+import * as S from "./styles";
+
 export function NavBar() {
-  return <aside>navbar</aside>;
+  return (
+    <S.Container>
+      <S.Logo>
+        <TvIcon />
+        <h4>TV Time</h4>
+      </S.Logo>
+      <S.Navigation>
+        <NavButton path="/" icon={<AllIcon />} label="Home" />
+        <NavButton icon={<MovieIcon />} label="Filme" />
+        <NavButton icon={<GenresIcon />} label="Gênero" />
+        <NavButton path="/cadastrar" icon={<EditorIcon />} label="Cadastrar" />
+      </S.Navigation>
+    </S.Container>
+  );
 }
---
+++ frontend/src/layouts/AppSkeleton/components/NavBar/styles.ts
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+
+export const Container = styled.aside`
+  padding: 30px 10px;
+  margin: -30px 0 -30px -30px;
+
+  width: 70px;
+
+  background: #211f41;
+`;
+
+export const Logo = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  svg {
+    width: 25px;
+    height: 25px;
+
+    fill: #fff;
+  }
+`;
+
+export const Navigation = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  gap: 20px;
+  margin-top: 30px;
+`;
