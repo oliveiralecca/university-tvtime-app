450944ce8017e489379f9fd79eedab389e4cf390
oliveiralecca <oliveiraslc@yahoo.com.br>
2022-11-12 22:24:23 -0300

    refactor(hooks-context): updates hooks and context

---
+++ frontend/src/hooks/useGetGenres.ts
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+import api from "../services/api";
+
+export type UseGetGenresResult = {
+  id_genero: number;
+  nome: string;
+  icone: string;
+};
+
+export function useGetGenres() {
+  const [genres, setGenres] = useState<UseGetGenresResult[]>([]);
+  const [isGenresLoading, setIsGenresLoading] = useState(true);
+
+  useEffect(() => {
+    api.get<UseGetGenresResult[]>("/filme/index").then((response) => {
+      setGenres(response.data);
+      setIsGenresLoading(false);
+    });
+  }, []);
+
+  return { genres, isGenresLoading };
+}
