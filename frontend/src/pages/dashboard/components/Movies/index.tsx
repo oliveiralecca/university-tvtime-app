7f2e0d2150b1eccaeac2b8a5be5fdd7d1e098ba2
oliveiralecca <oliveiraslc@yahoo.com.br>
2022-11-12 22:18:31 -0300

    refactor(movies-section): updates `Movies` section

---
+++ frontend/src/pages/dashboard/components/Movies/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import * as S from './styles';
+
+type MoviesProps = {};
+
+export function Movies() {
+  return (
+    <S.Container>
+      <p>filmessss</p>
+    </S.Container>
+  );
+}
