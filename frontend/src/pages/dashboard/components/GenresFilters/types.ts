96ccc601afda300e1bca854fbd4e0239840c06e8
oliveiralecca <oliveiraslc@yahoo.com.br>
2022-11-13 01:42:28 -0300

    feat: creates movie and filter types

---
+++ frontend/src/pages/dashboard/components/GenresFilters/types.ts
@@ -0,0 +1,19 @@
+export type Movie = {
+  id_video: number;
+  titulo: string;
+  tempo: string;
+  data_de_estreia: string;
+  resumo: string;
+  id_diretor: number | null;
+  id_filme: number;
+  capa: string | null;
+  titulos_equivalentes: string[];
+  id_estudio: number | null;
+};
+
+export type UseGetMoviesByGenreResult = {
+  id_genero: number;
+  nome: string;
+  icone: string;
+  filmes: Movie[];
+};
