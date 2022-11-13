db0644aadd40d42c70af62917dd4ec6c869271ee
oliveiralecca <oliveiraslc@yahoo.com.br>
2022-11-12 22:16:03 -0300

    refactor(components): excludes `Card` component

--- frontend/src/pages/movieDetails/index.tsx
+++ frontend/src/pages/movieDetails/index.tsx
@@ -1,6 +1,6 @@
 import { useParams } from "react-router-dom";
 import { Button } from "../../components/Button";
-import { Card } from "../../components/Card";
+// import { Card } from "../../components/GenreCard";
 import { Container } from "../../components/Container";
 import { useDataResults } from "../../contexts/dataContext";
 
@@ -8,14 +8,21 @@ export function MovieDetails() {
   const { users } = useDataResults();
   const { user } = useParams();
 
-  const userDetails = user ? users?.[+user] : undefined
-  const genderIdentify = userDetails?.gender.toUpperCase() === 'M' ? 'do' : 'da'
-  const firstName = userDetails?.name.split(' ')[0]
+  const userDetails = user ? users?.[+user] : undefined;
+  const genderIdentify =
+    userDetails?.gender.toUpperCase() === "M" ? "do" : "da";
+  const firstName = userDetails?.name.split(" ")[0];
 
   return (
     <Container>
-      {userDetails && <Card name={userDetails.name} age={userDetails.age} gender={userDetails.gender} />}
+      {/* {userDetails && (
+        <Card
+          name={userDetails.name}
+          age={userDetails.age}
+          gender={userDetails.gender}
+        />
+      )} */}
       <Button path="/" label="voltar" />
     </Container>
-  )
+  );
 }
