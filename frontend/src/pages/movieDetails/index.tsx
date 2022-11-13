2a6d4e91ba1592370b3fcf8b9ff7d2658206a1cd
Marcos Paulo <82289818+mpaullos@users.noreply.github.com>
2022-11-13 15:50:17 -0300

    update changes-movie

--- frontend/src/pages/movieDetails/index.tsx
+++ frontend/src/pages/movieDetails/index.tsx
@@ -3,7 +3,7 @@ import { Button } from "../../components/Button";
 // import { Card } from "../../components/GenreCard";
 import { Container } from "../../components/Container";
 import { useDataResults } from "../../contexts/dataContext";
-import * as S from "./styles"
+import * as S from "./styles";
 
 export function MovieDetails() {
   /* const { users } = useDataResults();
@@ -18,23 +18,30 @@ export function MovieDetails() {
     <Container>
       {/* {userDetails && <Card name={userDetails.name} age={userDetails.age} gender={userDetails.gender} />} */}
       <S.Container>
-       <div className="capa">
-        <div>
-          Capa
+        <div className="capa">
+          <div>
+            <img className="cover" src="https://posters.movieposterdb.com/21_07/2019/6472976/l_6472976_69f61b7f.jpg" alt="" />
+            <p>Data de Estreia: 15/03/2019</p>
+            <p>Duração: 1h56 min</p>
+          </div>
         </div>
-       </div>
-       <div className="description">
-        <div className="genero">
-         <h1>Venon 2: Let There Be Carnage</h1>
-        <button>Action</button>
-        <button>Ficcao</button>
-        <button>Adventure</button>
-        <button>Thriller</button>
+        <div className="description">
+          <div className="genero">
+            <h1>A cinco passos de me suicídar</h1>
+            <button>Action</button>
+            <button>Ficcao</button>
+            <button>Adventure</button>
+            <button>Thriller</button>
+          </div>
         </div>
-       </div>
-       <div className="title-geners">c</div>
+        <div className="title-geners">
+          <h4>Sinopse</h4>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. A laborum quo non laudantium voluptatum, tenetur placeat, tempora eum itaque ratione porro dolorem, commodi officiis animi et mollitia ea quia! Asperiores? At facilis minus explicabo ut excepturi, aperiam porro earum tempora, quas
+            numquam odit perspiciatis sit velit dicta ullam eos sunt? Animi, culpa alias distinctio ducimus porro incidunt reprehenderit obcaecati nihil!
+          </p>
+        </div>
       </S.Container>
-      
     </Container>
   );
 }
--- frontend/src/pages/movieDetails/styles.ts
+++ frontend/src/pages/movieDetails/styles.ts
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  background-color: red;
+  background-color: #3f3e6d;
   display: grid;
   width: 1100px;
   height: 450px;
@@ -12,7 +12,7 @@ export const Container = styled.div`
   grid-gap: 10px;
 
   .capa {
-    background-color: green;
+    background-color: #FE6737;
     width: 298px;
     height: 451px;
     margin-bottom: 20px;
@@ -20,37 +20,85 @@ export const Container = styled.div`
   }
 
   .title-geners {
-    background-color: blue;
+    background-color: #211F41;
     width: 767px;
     height: 200px;
     margin-bottom: -300px;
     grid-area: logo;
+    border-radius: 26px;
+    
+  }
+
+  .title-geners > h4,
+  p {
+    text-align: justify;
+    margin: 0 20px 10px 20px;
   }
 
   .description {
-    background-color: #211f41;
+    background-color: #211F41;
     width: 767px;
     height: 226px;
     margin-bottom: 30px;
+    border-radius: 38px;
+    
+    
   }
 
   .genero > button {
-    
     background-color: #3f3e6d;
-    border-color: white;
-    border-style: solid;
+    border-style: none;
     border-radius: 40px;
     border-width: 2px;
     width: 40px;
     height: 50px;
-    margin: 0 20px;
-    width: 128px;
-    height: 32px;
+    margin: 0 6px 1px;
+    width: 68px;
+    height: 28px;
     color: white;
   }
+
+
+  
 
   h1 {
     text-align: center;
     margin-bottom: 10px;
+    background-color: #FE6737;
+    border-top-left-radius: 38px;
+    border-top-right-radius: 38px;
+  }
+  
+  h4 {
+    padding-top: 10px;
+    
+  }
+
+  button:hover {
+    background-color: #FE6737;
+  }
+ 
+
+  .genero > .edit {
+    text-align: center;
+    width: 42px;
+    height: 42px;
+    background-color: #211f41;
+  }
+
+ 
+
+/*   .description button :hover {
+    background-color: orange;
+  } */
+
+  .cover {
+    width: 298px;
+    height: 360px;
+
+
+   
+
+
   }
 `;
