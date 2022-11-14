5bbba502e98f046b56635aff9b1043e01bd454ee
Marcos Paulo <82289818+mpaullos@users.noreply.github.com>
2022-11-14 04:12:27 -0300

    refactor(about-page): update page

--- frontend/src/pages/about/index.tsx
+++ frontend/src/pages/about/index.tsx
@@ -1,6 +1,42 @@
-export function About(
-) {
-    return (
-        <p>teste</p>
-    )
+import * as S from "./styles";
+export function About() {
+  return (
+    <S.Body>
+      <h1>Projeto da discplina Banco de Dados 2022.1</h1>
+      <div className="members">
+        <button>
+          <a href="https://github.com/oliveiralecca" target="_blank">
+            <img src="https://avatars.githubusercontent.com/u/65191407?v=4" alt="" />
+            <p>
+              Léticia Caroline
+              <br />
+              <img className="github" src="/src/assets/icons/github-icon.png" alt="" />
+            </p>
+          </a>
+        </button>
+        <button>
+          <a href="https://github.com/mpaullos" target="_blank">
+            <img src="https://avatars.githubusercontent.com/u/82289818?v=4" alt="" />
+            <p>
+              Marcos Paulo <br />
+              <img className="github" src="/src/assets/icons/github-icon.png" alt="" />
+            </p>
+          </a>
+        </button>
+        <button>
+          <a href="https://github.com/rodrigodesan" target="_blank">
+            <img src="https://avatars.githubusercontent.com/u/76262266?v=4" alt="" />
+            <p>
+              Rodrigo Nunes <br />
+              <img className="github" src="/src/assets/icons/github-icon.png" alt="" />
+            </p>
+          </a>
+        </button>
+      </div>
+
+      <div>
+        <p>Professor: André Brito</p>
+      </div>
+    </S.Body>
+  );
 }
--- frontend/src/pages/about/styles.ts
+++ frontend/src/pages/about/styles.ts
@@ -1,6 +1,53 @@
 import styled from "styled-components";
 
-export const Thousand = styled.div`
 
+export const Body = styled.body`
+  margin: auto;
+  padding: 0;
+  background-color: none;
+  text-align: center;
+  color: white;
 
-`
+  .members {
+    margin: 50px 0 40px 0px;
+  }
+
+  img {
+    width: 250px;
+    height: 250px;
+    margin-top: 5px;
+    margin-bottom: -3px;
+    border-top-left-radius: 28px;
+    border-top-right-radius: 28px;
+  }
+
+  button {
+    background-color: #3F3E6D;
+    border: none;
+    margin: 20px;
+  }
+
+  .github {
+    width: 38px;
+    height: 38px;
+    margin-top: 0px;
+    padding:2px;
+  }
+
+  p {
+    font-size: 24px;
+    color: white;
+    text-align: center;
+    
+  }
+
+  button  p {
+    background-color: #FE6737;
+    border-bottom-left-radius: 28px;
+    border-bottom-right-radius: 28px;
+}
+
+  a {
+    text-decoration: none;
+  }
+`;
