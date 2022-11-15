022c140c27058dcb404585b82dcb759400ca57d1
oliveiralecca <oliveiraslc@yahoo.com.br>
2022-11-15 02:15:03 -0300

    feat(icons): adds new icons

--- frontend/src/components/Icons/index.ts
+++ frontend/src/components/Icons/index.ts
@@ -1,19 +1,25 @@
+import { ReactComponent as About} from "../../assets/icons/about-icon.svg";
 import { ReactComponent as AllIcon } from "../../assets/icons/squares.svg";
 import { ReactComponent as ArrowLeftIcon } from "../../assets/icons/arrow-left.svg";
+import { ReactComponent as ArrowRightIcon } from "../../assets/icons/arrow-right.svg";
+import { ReactComponent as EditIcon } from "../../assets/icons/edit.svg";
 import { ReactComponent as EditorIcon } from "../../assets/icons/pencil.svg";
 import { ReactComponent as GenresIcon } from "../../assets/icons/ghost.svg";
 import { ReactComponent as MovieIcon } from "../../assets/icons/movie.svg";
 import { ReactComponent as NoResultIcon } from "../../assets/icons/magnifying-glass.svg";
+import { ReactComponent as TrashIcon } from "../../assets/icons/trash.svg";
 import { ReactComponent as TvIcon } from "../../assets/icons/tv.svg";
-import { ReactComponent as About} from "../../assets/icons/about-icon.svg";
 
 export {
+  About,
   AllIcon,
   ArrowLeftIcon,
+  ArrowRightIcon,
+  EditIcon,
   EditorIcon,
   GenresIcon,
   MovieIcon,
   NoResultIcon,
+  TrashIcon,
   TvIcon,
-  About
 };
