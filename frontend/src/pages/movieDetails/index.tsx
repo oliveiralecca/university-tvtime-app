9b65cc8d57f7a6ea374606b854cddad1d17d0d2a
Marcos Paulo <82289818+mpaullos@users.noreply.github.com>
2022-11-14 23:41:36 -0300

    feat(movieDetails): get genres, sinopse, date/time

--- frontend/src/pages/movieDetails/index.tsx
+++ frontend/src/pages/movieDetails/index.tsx
@@ -1,5 +1,6 @@
 import { Container } from "../../components/Container";
 import { Loader } from "../../components/Loader";
+import { MovieCard } from "../../components/MovieCard";
 import { useDataResults } from "../../contexts/dataContext";
 import * as S from "./styles";
 
@@ -14,9 +15,19 @@ export function MovieDetails() {
       <S.Container>
         <div className="capa">
           <div>
+          {/* {isMoviesDetailsLoading ? (
+              <Loader/>
+            ): (<p>{JSON.parse(JSON.stringify(moviesDetails?.capa))}</p>)} */}
             <img className="cover" src="https://posters.movieposterdb.com/21_07/2019/6472976/l_6472976_69f61b7f.jpg" alt="" />
-            <p>Data de Estreia: 15/03/2019</p>
-            <p>Duração: 1h56 min</p>
+            {isMoviesDetailsLoading ? (
+              <Loader/>
+            ): (<p>{JSON.parse(JSON.stringify(moviesDetails?.data_de_estreia))}
+                   {JSON.parse(JSON.stringify(moviesDetails?.tempo))}</p>)
+            
+            }
+            
+            {/* <p>Data de Estreia: 15/03/2019</p>
+            <p>Duração: 1h56 min</p> */}
           </div>
         </div>
         <div className="description">
@@ -25,18 +36,21 @@ export function MovieDetails() {
               <Loader/>
             ): (<p>{JSON.parse(JSON.stringify(moviesDetails?.titulo))}</p>)}
             </h1>
-            <button>Action</button>
+            {/* <button>Action</button>
             <button>Ficcao</button>
             <button>Adventure</button>
-            <button>Thriller</button>
+            <button>Thriller</button> */}
+            {isMoviesDetailsLoading ? (
+              <Loader/>
+            ): (<button>{JSON.stringify(moviesDetails?.generos)}</button>)
+            }
           </div>
         </div>
         <div className="title-geners">
           <h4>Sinopse</h4>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. A laborum quo non laudantium voluptatum, tenetur placeat, tempora eum itaque ratione porro dolorem, commodi officiis animi et mollitia ea quia! Asperiores? At facilis minus explicabo ut excepturi, aperiam porro earum tempora, quas
-            numquam odit perspiciatis sit velit dicta ullam eos sunt? Animi, culpa alias distinctio ducimus porro incidunt reprehenderit obcaecati nihil!
-          </p>
+          {isMoviesDetailsLoading ? (
+              <Loader/>
+            ): (<p>{JSON.parse(JSON.stringify(moviesDetails?.resumo))}</p>)}
         </div>
       </S.Container>
     </Container><S.Container style={{ background: "none" }}>
@@ -56,7 +70,7 @@ export function MovieDetails() {
         {isMoviesDetailsLoading ? (
           <Loader />
         ) : (
-          <p>{JSON.stringify(moviesDetails)}</p>
+          <p>{JSON.stringify(moviesDetails?.generos)}</p>
         )}
       </S.Container></>
   );
