284ce747a37bb0f55b7bee73fcf4a525595876de
oliveiralecca <oliveiraslc@yahoo.com.br>
2022-11-15 02:19:33 -0300

    feat(forms-pages): creates forms pages

---
+++ frontend/src/pages/editForm/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import * as S from './styles';
+
+type editFormProps = {};
+
+export function EditForm() {
+  return (
+    <S.Container>
+
+    </S.Container>
+  );
+}
---
+++ frontend/src/pages/registerForm/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import * as S from './styles';
+
+type registerFormProps = {};
+
+export function RegisterForm() {
+  return (
+    <S.Container>
+
+    </S.Container>
+  );
+}
