a3a17c767e21ab87667628816c37a5755c49d759
oliveiralecca <oliveiraslc@yahoo.com.br>
2022-11-17 13:39:09 -0300

    fix(images): changes images directory

--- frontend/src/components/Icons/index.ts
+++ frontend/src/components/Icons/index.ts
@@ -1,15 +1,15 @@
-import { ReactComponent as About} from "../../assets/icons/about-icon.svg";
-import { ReactComponent as AllIcon } from "../../assets/icons/squares.svg";
-import { ReactComponent as ArrowLeftIcon } from "../../assets/icons/arrow-left.svg";
-import { ReactComponent as ArrowRightIcon } from "../../assets/icons/arrow-right.svg";
-import { ReactComponent as CancelIcon } from "../../assets/icons/cancel.svg";
-import { ReactComponent as EditIcon } from "../../assets/icons/edit.svg";
-import { ReactComponent as EditorIcon } from "../../assets/icons/pencil.svg";
-import { ReactComponent as GenresIcon } from "../../assets/icons/ghost.svg";
-import { ReactComponent as MovieIcon } from "../../assets/icons/movie.svg";
-import { ReactComponent as NoResultIcon } from "../../assets/icons/magnifying-glass.svg";
-import { ReactComponent as TrashIcon } from "../../assets/icons/trash.svg";
-import { ReactComponent as TvIcon } from "../../assets/icons/tv.svg";
+import { ReactComponent as About} from "../../../public/assets/icons/about-icon.svg";
+import { ReactComponent as AllIcon } from "../../../public/assets/icons/squares.svg";
+import { ReactComponent as ArrowLeftIcon } from "../../../public/assets/icons/arrow-left.svg";
+import { ReactComponent as ArrowRightIcon } from "../../../public/assets/icons/arrow-right.svg";
+import { ReactComponent as CancelIcon } from "../../../public/assets/icons/cancel.svg";
+import { ReactComponent as EditIcon } from "../../../public/assets/icons/edit.svg";
+import { ReactComponent as EditorIcon } from "../../../public/assets/icons/pencil.svg";
+import { ReactComponent as GenresIcon } from "../../../public/assets/icons/ghost.svg";
+import { ReactComponent as MovieIcon } from "../../../public/assets/icons/movie.svg";
+import { ReactComponent as NoResultIcon } from "../../../public/assets/icons/magnifying-glass.svg";
+import { ReactComponent as TrashIcon } from "../../../public/assets/icons/trash.svg";
+import { ReactComponent as TvIcon } from "../../../public/assets/icons/tv.svg";
 
 export {
   About,
