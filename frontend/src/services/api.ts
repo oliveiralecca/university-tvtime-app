09d2d5594a9962ad569cf90f8cfafe347471e64a
oliveiralecca <oliveiraslc@yahoo.com.br>
2022-11-17 12:58:05 -0300

    build: changes base url api

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: import.meta.env.VITE_API_URL,
-  baseURL: 'http://localhost:3030',
+  baseURL: import.meta.env.VITE_API_URL,
 })
 
 export default api;
