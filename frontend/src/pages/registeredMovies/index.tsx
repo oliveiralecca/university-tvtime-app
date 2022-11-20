a179113053924c25cd9526796b552beb6d385322
oliveiralecca <oliveiraslc@yahoo.com.br>
2022-11-20 01:54:01 -0300

    fix: adjust toast time

--- frontend/src/pages/registeredMovies/index.tsx
+++ frontend/src/pages/registeredMovies/index.tsx
@@ -24,7 +24,7 @@ export function RegisteredMovies() {
   const notifyError = (msg: string) => toast.error(msg, { autoClose: 3000 });
 
   async function onDelete(id: number) {
-    toast.info('Excluindo o filme...', { autoClose: 1500 });
+    toast.info('Excluindo o filme...', { autoClose: 2000 });
     const response = await api.delete(`/filme/delete/${id}`);
     if (response.data.success) {
       notifySuccess(response.data.success);
