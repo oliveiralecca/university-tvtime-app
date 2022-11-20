8cecf1e35022b4dcdbac479427dbb8db9782e07e
oliveiralecca <oliveiraslc@yahoo.com.br>
2022-11-20 02:25:07 -0300

    feat(movie-detail): adds tittulos equivalentes

--- frontend/src/pages/movieDetails/index.tsx
+++ frontend/src/pages/movieDetails/index.tsx
@@ -42,14 +42,38 @@ export function MovieDetails() {
               <h1>{moviesDetails?.titulo}</h1>
               <div>
                 {moviesDetails?.generos.map((gen) => (
-                  <button key={gen.id_genero}>{genreNameTranslate(gen.nome)}</button>
+                  <button key={gen.id_genero}>
+                    {genreNameTranslate(gen.nome)}
+                  </button>
                 ))}
               </div>
             </div>
           </div>
           <div className="title-geners">
             <h4>Sinopse</h4>
-            <p>{moviesDetails?.resumo}</p>
+            <p>
+              {moviesDetails?.resumo ? (
+                moviesDetails.resumo
+              ) : (
+                <i>Nenhuma sinopse cadastrada</i>
+              )}
+            </p>
+            <br />
+            <h4>Títulos equivalentes</h4>
+            <p>
+              {moviesDetails?.titulos_equivalentes.length ? (
+                moviesDetails?.titulos_equivalentes.map(
+                  (item, idx) =>
+                    `${item}${
+                      idx !== moviesDetails?.titulos_equivalentes.length - 1
+                        ? ", "
+                        : ""
+                    }`
+                )
+              ) : (
+                <i>Nenhum título equivalente cadastrado</i>
+              )}
+            </p>
           </div>
         </S.Container>
       )}
